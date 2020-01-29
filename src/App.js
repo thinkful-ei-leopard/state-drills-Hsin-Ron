6c335cb0a053030c020093624f9e2798ab0f9fe5
Ron Martin <ronmartin89@gmail.com>
2020-01-29 12:00:44 -0800

    added RouletteGun component to App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import HelloWorld from './state-drills/HelloWorld'
 import Bomb from './state-drills/Bomb';
 import './App.css';
+import RouletteGun from './state-drills/RouletteGun';
 
 function App() {
   return (
     <main className='App'>
       <HelloWorld />
       <Bomb />
+      <RouletteGun bulletInChamber={8} />
     </main>
   );
 }
