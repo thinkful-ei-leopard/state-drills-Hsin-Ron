1b8bfb4714bff571fadfe081df954a09c7484d9a
Ron Martin <ronmartin89@gmail.com>
2020-01-29 13:23:52 -0800

    Passed sections prop to Accordion and verified it didnt break

--- src/App.js
+++ src/App.js
@@ -3,13 +3,46 @@ import HelloWorld from './state-drills/HelloWorld'
 import Bomb from './state-drills/Bomb';
 import './App.css';
 import RouletteGun from './state-drills/RouletteGun';
+import Accordion from './state-drills/Accordion';
+
+const sections = [
+  {
+    title: 'Section 1',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    title: 'Section 2',
+    content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+  },
+  {
+    title: 'Section 3',
+    content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+  },
+]
 
 function App() {
+  const sections = [
+    {
+      title: 'Section 1',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    },
+    {
+      title: 'Section 2',
+      content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+    },
+    {
+      title: 'Section 3',
+      content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+    },
+  ]
+  
+
   return (
     <main className='App'>
       <HelloWorld />
       <Bomb />
       <RouletteGun bulletInChamber={8} />
+      <Accordion sections={sections} />
     </main>
   );
 }
---
+++ src/state-drills/Accordion.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+export default class Accordion extends React.Component {
+    
+    state = {
+
+    };
+
+
+    render() {
+        return (
+        <div>HELLO</div>
+        );
+    }
+}
