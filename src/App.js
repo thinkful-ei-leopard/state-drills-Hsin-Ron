1ad545cf8f8b41530d09f29b82e065992141910f
Ron Martin <ronmartin89@gmail.com>
2020-01-29 09:33:32 -0800

    Adjust App.js so that it will load our new components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import Split from './composition/Split';
+import HelloWorld from './state-drills/HelloWorld'
+import Bomb from './state-drills/Bomb';
 import './App.css';
 
 function App() {
   return (
     <main className='App'>
-      <Split className='left' flexBasis={2}>
-        This is the content for the left `Split`. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?
-      </Split>
-      <Split className='right' flexBasis={3}>
-        This is the content for the right `Split`. Inventore aliquid cupiditate suscipit repellat. Quaerat quis officiis quam fuga. Aliquid quo possimus id soluta aspernatur.
-      </Split>
+      <HelloWorld />
+      <Bomb />
     </main>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import App from './App';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-=======
-import './index.css';
-import HelloWorld from './state-drills/HelloWorld';
-
-
-ReactDOM.render(<HelloWorld />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-
->>>>>>> df91d9bb7e20f3d9ce58100c63c72e7b10765083
